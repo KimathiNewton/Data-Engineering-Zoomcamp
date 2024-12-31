7082ae6604376ec208d7ea46f5c31d8515319cc3
KimathiNewton <newtonkimathi20@gmail.com>
2024-12-31 11:51:44 +0300

    Simple pipeline

--- pipeline.py
+++ pipeline.py
@@ -1,26 +1,7 @@
-services:
-  postgres:
-    image: postgres:13
-    environment:
-      POSTGRES_USER: airflow
-      POSTGRES_PASSWORD: airflow
-      POSTGRES_DB: airflow
-    volumes:
-      - postgres-db-volume:/var/lib/postgresql/data
-    healthcheck:
-      test: ["CMD", "pg_isready", "-U", "airflow"]
-      interval: 5s
-      retries: 5
-    restart: always
+import sys
 
+day = sys.argv[1]
+time = sys.argv[2]
 
-
-
-mintty docker run -t \
-    -e POSTGRES_USER = "root" \
-    -e POSTGRES_PASSWORD = "root" \
-    -e POSTGRES_DB = "ny_taxi" \
-    -v c:/Users/USER/Projects/Data_Engineering/ny_taxi_postgres_data:/var/lib/postgresql/data
-    -p 5432:5432 \
-    postgress:13
+print(f"Job finished at {time},{day}")
  
